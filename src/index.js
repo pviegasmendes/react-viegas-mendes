a6475e234677a26827eb0b2c14dbc2aec30e21bc
Pablo Viegas Mendes <pviegasmendes@gmail.com>
2022-07-21 16:24:06 -0300

    se implementa sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./componets/App";
-import "./style.css";
+import "./style.scss";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
