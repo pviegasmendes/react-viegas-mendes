a97fb936c2c795d5ee2b980376257893ed74e871
Pablo Viegas Mendes <pviegasmendes@gmail.com>
2022-08-30 17:27:23 -0300

    some english translations

--- src/componets/Cart.js
+++ src/componets/Cart.js
@@ -12,18 +12,18 @@ const Cart = () => {
 
     return cartLength === 0 ? (
         <div className="grid justify-center">
-            <h1 className="font-bold text-xl m-4">Carrito Vacio</h1>
+            <h1 className="font-bold text-xl m-4">you Cart Is Empty</h1>
             <Link
                 to={"/"}
                 className="font-bold mt-8 p-4 mb-2 border-solid border-2 text-white bg-violet-500  rounded-xl hover:bg-violet-700 "
             >
-                Ver Productos
+                Go Shopping!
             </Link>
         </div>
     ) : (
         <div className="grid lg:grid-cols-6 p-9">
             <div className="col-span-4">
-                <h2>Productos en carrito</h2>
+                <h2>Cart Products</h2>
                 <div>
                     {cart.map((product) => (
                         <ItemCart key={product.id} product={product} />
