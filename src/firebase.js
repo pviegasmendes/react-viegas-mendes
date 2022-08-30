901dd1a40b351fea7e3d29ab75020dbc11962b5c
Pablo Viegas Mendes <pviegasmendes@gmail.com>
2022-08-30 17:08:19 -0300

    privateKey enviroment

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDROYmMFMsSeGmKHbWii2Rl-hAFvaaGuQM",
+    apiKey: process.env.REACT_APP_privateKey,
     authDomain: "ecommerce-4d74f.firebaseapp.com",
     projectId: "ecommerce-4d74f",
     storageBucket: "ecommerce-4d74f.appspot.com",
